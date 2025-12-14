f6ba515512d191c60a3f30f44daacd68f68356bd
Chamidu Dilshan <chamidudilshan0423@gmail.com>
2025-12-14 18:30:47 +0530

    Implement email connection functionality with UI components for login

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,15 +1,19 @@
 mod db;
+mod email;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     match db::init() {
         Ok(_) => println!("Database initialized successfully!"),
-        Err(e) => eprintln!("Database Error: {}", e),
+        Err(e) => eprintln!("Database Error: {}", e ),
     }
 
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_notification::init())
+        .invoke_handler(tauri::generate_handler![
+            email::test_imap_connection
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
